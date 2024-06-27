54430308ae0e9232c73b0d6c13d1011f972be96e
ekamsinghahuja <ekamsinghahuja123@gmail.com>
2024-06-27 19:50:41 +0530

    ll

--- src/components/Welcom.jsx
+++ src/components/Welcom.jsx
@@ -4,7 +4,8 @@ import logo from "../Assets/logoo-removebg-preview.png"
 const Welcom = () => {
   return (
     <div className='welcom-container'>
-        <img src={logo} alt="" />
+        <img src={logo} alt="" className='welcom-logo'/>
+        <p>view and text directly to people prsent in chat room</p>
 
       
     </div>
